3e056f37f9ee3f52155787ded9888bd34da17dea
granny <granny@pl3x.net>
2022-08-07 17:56:13 -0700

    scroll to a given anchor using js

---
+++ overrides/js/meta.js
@@ -0,0 +1,34 @@
+if (location.pathname.toLowerCase().includes('configuration')) {
+  //const options = { behavior: "smooth", block: "start", inline: "nearest" };
+  const yOffset = -75;
+
+  /* anchors */
+  for (const anchorElement of document.getElementsByClassName('headerlink')) {
+    anchorElement.addEventListener('click', (event) => {
+      event.preventDefault();
+      const anchor = anchorElement.getAttribute('href');
+      console.log("Anchor: " + anchor);
+
+      window.history.pushState(null, '', anchor);
+      const y = anchorElement.parentElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({top: y, behavior: 'smooth'});
+      //anchorElement.parentElement.scrollIntoView(options);
+    });
+  }
+
+  /* side bar */
+  for (const anchorElement of document.getElementsByClassName('md-nav__link')) {
+    anchorElement.addEventListener('click', (event) => {
+      event.preventDefault();
+      const anchor = anchorElement.getAttribute('href');
+      console.log("Anchor: " + anchor);
+
+      const headerElement = document.getElementById(anchorElement.innerText);
+
+      window.history.pushState(null, '', anchor);
+      const y = headerElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({top: y, behavior: 'smooth'});
+      //headerElement.scrollIntoView(options);
+    });
+  }
+}
